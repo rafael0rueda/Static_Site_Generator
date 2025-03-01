daa4a603082a51a0640f3b0878d29a32ff96db73
rafael0rueda <rrrm85@gmail.com>
2025-03-01 15:28:21 -0500

    Add clean and copy from static to public

--- src/main.py
+++ src/main.py
@@ -1,6 +1,35 @@
 from textnode import *
+import os
+import shutil
+
+def delete_files_in_directory(directory_path):
+    files = os.listdir(directory_path)
+    for file in files:
+        sub_folder = os.path.join(directory_path, file)
+        if os.path.isdir(sub_folder):
+            shutil.rmtree(sub_folder)
+        else:
+            os.unlink(sub_folder)
+    
+    return
+
+def clean_copy_directory(dir_src, dir_dest):
+    delete_files_in_directory(dir_dest)
+    
+    files = os.listdir(dir_src)
+    for file in files:
+        s = os.path.join(dir_src, file)
+        d = os.path.join(dir_dest, file)
+        if os.path.isdir(s):
+            shutil.copytree(s, d)
+        else:
+            shutil.copy2(s, d)
+    return
 
 def main():
+    src_dir = "/home/rafa/Bootdev/workspace/github.com/rafael0rueda/ssg/static"
+    dest_dir = "/home/rafa/Bootdev/workspace/github.com/rafael0rueda/ssg/public/"
+    clean_copy_directory(src_dir, dest_dir)
     new_node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(new_node)
     return
